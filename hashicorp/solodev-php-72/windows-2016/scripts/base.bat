3d72b33322b64b04a8614648ef446677f9067054
techcto <smoore@solodev.com>
2019-06-16 17:06:19 -0400

    ..

--- hashicorp/solodev-php-72/windows-2016/scripts/base.bat
+++ hashicorp/solodev-php-72/windows-2016/scripts/base.bat
@@ -5,11 +5,11 @@ echo ---------------------
 
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install IIS-WebServerRole -source windowsfeatures
-choco install IIS-ISAPIFilter -source windowsfeatures
-choco install IIS-ISAPIExtensions -source windowsfeatures
-choco install IIS-NetFxExtensibility -source windowsfeatures
-choco install IIS-CGI -source windowsfeatures
+choco install IIS-WebServerRole --source WindowsFeatures
+choco install IIS-ISAPIFilter --source WindowsFeatures
+choco install IIS-ISAPIExtensions --source WindowsFeatures
+choco install IIS-NetFxExtensibility --source WindowsFeatures
+choco install IIS-CGI --source WindowsFeatures
 choco install urlrewrite -y
 choco install vcredist2012 -y
 choco install vcredist2013 -y
