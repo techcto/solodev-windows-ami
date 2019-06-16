9eba80b668d56be847d60dd799262dd1c4b1645a
techcto <smoore@solodev.com>
2019-06-16 15:23:43 -0400

    ..

--- hashicorp/solodev-php-72/windows-2016/SetUpWinRM.ps1
+++ hashicorp/solodev-php-72/windows-2016/SetUpWinRM.ps1
@@ -19,19 +19,19 @@ write-output "Setting up WinRM"
 write-host "(host) setting up WinRM"
 
 cmd.exe /c winrm quickconfig -q
-cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
-cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="4096"}'
-cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"} Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled false'
-cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
-# cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
-cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
+cmd.exe /c winrm quickconfig -transport:http
+cmd.exe /c winrm set winrm/config @{MaxTimeoutms="1800000"}
+cmd.exe /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
+cmd.exe /c winrm set winrm/config/service @{AllowUnencrypted="true"}
+cmd.exe /c winrm set winrm/config/service/auth @{Basic="true"}
+cmd.exe /c winrm set winrm/config/client/auth @{Basic="true"}
+cmd.exe /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5986"}
+cmd.exe /c netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" profile=public protocol=tcp localport=5985 remoteip=localsubnet new remoteip=any
+cmd.exe /c net stop winrm 
+cmd.exe /c sc config winrm start= auto
+cmd.exe /c net start winrm 
 
 cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
 cmd.exe /c netsh firewall add portopening TCP 5986 "Port 5986"
-cmd.exe /c net stop winrm
-cmd.exe /c sc config winrm start= auto
-cmd.exe /c net start winrm
 
 </powershell>
