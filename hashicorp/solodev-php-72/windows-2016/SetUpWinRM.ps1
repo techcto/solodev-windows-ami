e970e2ccb5dc6ce0dbd56d62afedd6e1e5465841
techcto <smoore@solodev.com>
2019-06-16 15:25:02 -0400

    ..

--- hashicorp/solodev-php-72/windows-2016/SetUpWinRM.ps1
+++ hashicorp/solodev-php-72/windows-2016/SetUpWinRM.ps1
@@ -25,13 +25,11 @@ cmd.exe /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
 cmd.exe /c winrm set winrm/config/service @{AllowUnencrypted="true"}
 cmd.exe /c winrm set winrm/config/service/auth @{Basic="true"}
 cmd.exe /c winrm set winrm/config/client/auth @{Basic="true"}
-cmd.exe /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5986"}
+cmd.exe /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
 cmd.exe /c netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" profile=public protocol=tcp localport=5985 remoteip=localsubnet new remoteip=any
 cmd.exe /c net stop winrm 
 cmd.exe /c sc config winrm start= auto
 cmd.exe /c net start winrm 
-
-cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
-cmd.exe /c netsh firewall add portopening TCP 5986 "Port 5986"
+cmd.exe /c netsh firewall add portopening TCP 5985 "Port 5985"
 
 </powershell>
