aa9c159a5d31a4c6e8df05618a62c6527390b40f
techcto <smoore@solodev.com>
2019-06-16 12:36:30 -0400

    ..

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+# Init
 # TIME_DIFF_LATEST_FILE=$(($(date +%s)-$(find hashicorp/solodev-php-72/windows-2016 -printf "%Ts\n" | sort | tail -1)))
 # if [ "$FORCE_BUILDS" = true ] || [ $TIME_DIFF_LATEST_FILE -lt 3600 ] ; then
     echo "Validating - $2"
