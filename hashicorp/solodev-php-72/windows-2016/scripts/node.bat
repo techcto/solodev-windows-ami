b8dd3fb1ab69864d376cf36dac1ae553a6f05f73
techcto <smoore@solodev.com>
2019-06-17 13:14:30 -0400

    ..

--- hashicorp/solodev-php-72/windows-2016/scripts/node.bat
+++ hashicorp/solodev-php-72/windows-2016/scripts/node.bat
@@ -3,4 +3,4 @@ echo ---------------------
 echo Installing Node
 echo ---------------------
 
-choco install urlrewrite vcredist2012 vcredist2013 awscli nodejs-lts googlechrome 7zip -y
+choco install nodejs-lts -y
