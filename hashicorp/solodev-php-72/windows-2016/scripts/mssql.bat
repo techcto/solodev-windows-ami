32e57aa6ebdac024fb1085b177cd4e5ac3d02646
techcto <smoore@solodev.com>
2019-06-17 17:56:53 -0400

    ..

--- hashicorp/solodev-php-72/windows-2016/scripts/mssql.bat
+++ hashicorp/solodev-php-72/windows-2016/scripts/mssql.bat
@@ -3,7 +3,8 @@ echo ---------------------
 echo Installing MSSql
 echo ---------------------
 
-choco install mssqlserver2014express -y --cachelocation c:\temp\choco
+REM choco install mssqlserver2014express -y --cachelocation c:\temp\choco
+choco install mssql2014express-defaultinstance -y
 
 @powershell [Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -OutFile msphpsql.zip https://windows.php.net/downloads/pecl/releases/pdo_sqlsrv/5.6.1/php_pdo_sqlsrv-5.6.1-7.2-nts-vc15-x64.zip
 7z x msphpsql.zip -o.\msphpsql
