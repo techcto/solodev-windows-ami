d2004cb8ca8562c9310241be6f6b7fcfca0e0d45
techcto <smoore@solodev.com>
2019-06-17 10:16:42 -0400

    ..

--- hashicorp/solodev-php-72/windows-2016/scripts/solodev.bat
+++ hashicorp/solodev-php-72/windows-2016/scripts/solodev.bat
@@ -1,6 +1,5 @@
 mkdir c:\Solodev
 
-#Install latest official release of Solodev
 fn="$(aws s3 ls s3://solodev-release | sort | tail -n 1 | awk '{print $4}')"
 aws s3 cp s3://solodev-release/$fn c:\Solodev\Solodev.zip
 
