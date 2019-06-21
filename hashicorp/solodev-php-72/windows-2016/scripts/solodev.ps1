e7170beaaabbf51580cfef11526a5623cb80ed94
techcto <smoore@solodev.com>
2019-06-21 07:50:54 -0400

    add checks, fixed rm zip

--- hashicorp/solodev-php-72/windows-2016/scripts/solodev.ps1
+++ hashicorp/solodev-php-72/windows-2016/scripts/solodev.ps1
@@ -4,6 +4,7 @@ cd c:\inetpub\Solodev
 iisreset /stop
 
 echo "" > scmd.bat
+echo 'rm Solodev.zip 2> NUL' >> scmd.bat
 echo "for /f %%i in ('aws s3 ls solodev-release ^| find /c /v """"') do set /a RELEASE_LENGTH=%%i-1" >> scmd.bat
 echo "for /f ""tokens=4"" %%A in ('aws s3 ls solodev-release ^| sort ^| more /T2 +%%RELEASE_LENGTH%%') do set S3_KEY=%%A" >> scmd.bat
 echo "aws s3 cp s3://solodev-release/%S3_KEY% c:\inetpub\Solodev\Solodev.zip" >> scmd.bat
@@ -12,19 +13,18 @@ echo '@powershell Remove-Item -path c:\inetpub\Solodev\new -recurse -ErrorAction
 echo '@powershell Remove-Item -path c:\inetpub\Solodev\old -recurse -ErrorAction Ignore' >> scmd.bat
 echo 'mkdir c:\inetpub\Solodev\new' >> scmd.bat
 echo 'mkdir c:\inetpub\Solodev\old' >> scmd.bat
-echo 'rm Solodev.zip 2> NUL' >> scmd.bat
 echo "7z x Solodev.zip -oc:\inetpub\Solodev\new\ * -r" >> scmd.bat
 echo "del c:\inetpub\Solodev\Solodev.zip" >> scmd.bat
-echo "move core old/" >> scmd.bat
-echo "move modules old/" >> scmd.bat
-echo "move public old/" >> scmd.bat
-echo "move vendor old/" >> scmd.bat
-echo "move license old/" >> scmd.bat
-echo "move tests old/" >> scmd.bat
-echo "move composer.json old/" >> scmd.bat
-echo "move composer.lock old/" >> scmd.bat
-echo "move license.txt old/" >> scmd.bat
-echo "move version.txt old/" >> scmd.bat
+echo 'if exist "c:\inetpub\Solodev\core" move core old/' >> scmd.bat
+echo 'if exist "c:\inetpub\Solodev\modules" move modules old/' >> scmd.bat
+echo 'if exist "c:\inetpub\Solodev\public" move public old/' >> scmd.bat
+echo 'if exist "c:\inetpub\Solodev\vendor" move vendor old/' >> scmd.bat
+echo 'if exist "c:\inetpub\Solodev\license" move license old/' >> scmd.bat
+echo 'if exist "c:\inetpub\Solodev\tests" move tests old/' >> scmd.bat
+echo 'if exist "c:\inetpub\Solodev\composer.json" move composer.json old/' >> scmd.bat
+echo 'if exist "c:\inetpub\Solodev\composer.lock" move composer.lock old/' >> scmd.bat
+echo 'if exist "c:\inetpub\Solodev\license.txt" move license.txt old/' >> scmd.bat
+echo 'if exist "c:\inetpub\Solodev\version.txt" move version.txt old/' >> scmd.bat
 echo "cd new" >> scmd.bat
 echo "move core ../" >> scmd.bat
 echo "move modules ../" >> scmd.bat
