d8d1d0d674ee8eb9680350ebbe96816c9fdff620
techcto <smoore@solodev.com>
2020-01-31 10:04:21 -0500

    ..

--- hashicorp/solodev-php-72/windows-2016/scripts/node.bat
+++ hashicorp/solodev-php-72/windows-2016/scripts/node.bat
@@ -4,3 +4,10 @@ echo Installing Node
 echo ---------------------
 
 choco install nodejs-lts -y
+
+mkdir "C:\inetpub\node_modules_global"
+npm config set prefix "C:\inetpub\node_modules_global"
+npm install -g --unsafe-perm @fortawesome/fontawesome-free autoprefixer clean-css-cli node-sass npm-run-all postcss postcss-cli gulp gulp-autoprefixer \
+		gulp-clean gulp-clean-css gulp-concat gulp-rename gulp-sass gulp-uglify node-sass
+
+mkdir "C:\inetpub\.npm"
