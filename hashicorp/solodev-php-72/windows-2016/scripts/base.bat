3ff8f2b0d740d72dd8eb0fccb72848f87c25314f
Ross Williams <ross@solodev.com>
2020-12-16 12:49:40 -0500

    fix to choco install

--- hashicorp/solodev-php-72/windows-2016/scripts/base.bat
+++ hashicorp/solodev-php-72/windows-2016/scripts/base.bat
@@ -3,8 +3,7 @@ echo ---------------------
 echo Installing Base
 echo ---------------------
 
-@powershell [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+@powershell [Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install IIS-WebServerRole IIS-ISAPIFilter IIS-ISAPIExtensions IIS-CGI --source WindowsFeatures
 choco install urlrewrite vcredist2012 vcredist2013 awscli googlechrome 7zip openssl.light -y
